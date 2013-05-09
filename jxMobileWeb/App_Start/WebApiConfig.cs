02b1c788e6aae3f9bb4e29a4d30b501fd20c81e9
ZhouYi <zhouyi@liandisys.com.cn>
2013-05-09 18:27:20 +0900

    namespace modification
    namespace modification

--- jxMobileWeb/App_Start/WebApiConfig.cs
+++ jxMobileWeb/App_Start/WebApiConfig.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
 
-namespace jxMobileWeb
+namespace JXM.MobileWeb
 {
     public static class WebApiConfig
     {
--- jxMobileWeb/Controllers/HomeController.cs
+++ jxMobileWeb/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace jxMobileWeb.Controllers
+namespace JXM.MobileWeb.Controllers
 {
     public class HomeController : Controller
     {
