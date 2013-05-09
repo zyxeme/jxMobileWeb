06ac20aab399df52a4c84c9331474383e9f80506
ZhouYi <zhouyi@liandisys.com.cn>
2013-05-09 22:08:43 +0900

    add RecordRepository
    add RecordRepository

---
+++ Domain/Interfaces/IRecordRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JXM.Data;
+
+namespace JXM.Domain.Interfaces
+{
+    public interface IRecordRepository
+    {
+        IList<vReportListForSearch> GetList();
+        vReportListForSearch GetById();
+        vReportListForSearch GetNewOne();
+        vReportListForSearch AddCarMain();
+    }
+}
---
+++ Domain/LocalDB/RecordRepositoryLocal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JXM.Data;
+using JXM.Domain.Interfaces;
+using JXM.Domain.LocalDB.Infrastructure;
+
+namespace JXM.Domain.LocalDB
+{
+    public class RecordRepositoryLocal : GenericRepository, IRecordRepository
+    {
+        public IList<Data.vReportListForSearch> GetList()
+        {
+            return GetQuery<vReportListForSearch>().ToList();
+        }
+
+        public Data.vReportListForSearch GetById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Data.vReportListForSearch GetNewOne()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Data.vReportListForSearch AddCarMain()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
