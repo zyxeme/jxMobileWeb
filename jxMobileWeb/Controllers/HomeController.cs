b04fe97f69f9e02e903a0ea352b8d93c87e742b8
ZhouYi <zhouyi@liandisys.com.cn>
2013-05-10 11:24:06 +0900

    add IOC(spring.core) support
    add IOC(spring.core) support

--- jxMobileWeb/Controllers/HomeController.cs
+++ jxMobileWeb/Controllers/HomeController.cs
@@ -4,13 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using JXM.Data;
+using JXM.Domain.Interfaces;
 using JXM.Domain.LocalDB;
 using JXM.Domain.LocalDB.Infrastructure;
+using JXM.MobileWeb.Infrastructure;
 
 namespace JXM.MobileWeb.Controllers
 {
-    public class HomeController : Controller
+    public class HomeController : BaseController
     {
+        private IRecordRepository recordRepository;
+
+        public HomeController() : this((IRecordRepository)context.GetObject("RecordRepository"))
+        {
+        }
+
+        public HomeController(IRecordRepository repository)
+        {
+            this.recordRepository = repository;
+        }
+
         //
         // GET: /Home/
 
@@ -22,8 +35,10 @@ namespace JXM.MobileWeb.Controllers
             //GenericRepository repo = new GenericRepository();
             //IList<vReportListForSearch> records = repo.GetQuery<vReportListForSearch>().ToList();
 
-            RecordRepositoryLocal repoLocal = new RecordRepositoryLocal();
-            IList<vReportListForSearch> records = repoLocal.GetList();
+            //RecordRepositoryLocal repoLocal = new RecordRepositoryLocal();
+            //IList<vReportListForSearch> records = repoLocal.GetList();
+
+            IList<vReportListForSearch> records = recordRepository.GetList();
 
             return View(records);
         }
---
+++ jxMobileWeb/Infrastructure/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace JXM.MobileWeb.Infrastructure
+{
+    public class BaseController : Controller
+    {
+        protected static IApplicationContext context = new XmlApplicationContext(System.Web.HttpContext.Current.Server.MapPath(@"~/Objects.config"));
+        protected static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public virtual ActionResult MenuButton(string ButtonId, string RouteMenu, string selectedIndex = "")
+        {
+            return Redirect("./Index");
+        }
+    }
+}
